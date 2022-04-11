9862407e70270f5b16e965f62807a226c84ceb49
YazanKhalilYahya <97372408+YazanKhalilYahya@users.noreply.github.com>
2022-04-11 19:41:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// first image
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+
+// second image
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+// title
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
